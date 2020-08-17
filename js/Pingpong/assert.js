baa3db5a7aaada5030767b4864b6f36b8317d4dc
napasun <wkzkfmxk23@gmail.com>
2020-08-17 23:24:31 +0900

    파일 정

---
+++ js/Pingpong/assert.js
@@ -0,0 +1,11 @@
+(function (lib, cjs) {
+	var p;
+	
+	(lib.Backgroud = function() {
+		this.initialize();
+		
+		this.gameScreen = new cjs.shape();
+	}).prototype = p = new cjs.Container();
+	
+})(lib = lib||{}, createjs = createjs||{});
+var lib, createjs;
